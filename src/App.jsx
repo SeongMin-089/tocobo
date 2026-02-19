6b241907287fffa50d615abef098b618e107d937
unknown <a99701841@gmail.com>
2026-02-19 14:52:29 +0900

    update

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import './styles/main.scss'
+import Header from './components/Header'
+import Hero from './sections/Hero'
+import Footer from './components/Footer'
+import Follow from './sections/Follow'
+import Collection from './sections/Collection'
+import SkinCare from './sections/SkinCare'
+import Instagram from './sections/instagram'
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      <main>
+        <section id="hero" className='section'>
+          <Hero/>
+        </section>
+
+        <section id="follow" className='section'>
+          <Follow/>
+        </section>
+
+        <section id="collection" className='section'>
+          <Collection/>
+        </section>
+
+        <section id="skincare" className='section'>
+          <SkinCare/>
+        </section>
+
+        <section id="instagram" className='section'>
+          <Instagram/>
+        </section>
+      </main>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/sections/Follow.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cta from './Cta'
+
+const Follow = () => {
+  return (
+    <div>
+        <Cta/>
+    </div>
+  )
+}
+
+export default Follow
