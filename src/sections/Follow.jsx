e2e788600607cc9775d51cb71fcc35a0804024b6
unknown <a99701841@gmail.com>
2026-02-24 14:40:29 +0900

    update

--- src/sections/Follow.jsx
+++ src/sections/Follow.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Hello from '../components/Hello'
 import Cta from '../components/Cta'
 const Follow = () => {
   return (
     <div>
         <Cta/>
+        <Hello/>
     </div>
   )
 }
