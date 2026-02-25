475d2dd8b16da1e167484812c57d0ec6ccf553a5
unknown <a99701841@gmail.com>
2026-02-25 16:01:26 +0900

    update

--- src/sections/Instargram.jsx
+++ src/sections/Instargram.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import instar from "../util/instargram";
+import './styles/instargram.scss'
 const Instargram = () => {
   return (
-    <div>Instargram</div>
-  )
-}
+    <div className="inner instar-inner">
+      <div className="t-wrap">
+        <h2 className="tit">Instagram</h2>
+        <p className="txt">@tocobo_official</p>
+      </div>
+      <ul className="instar-list">
+        {instar.map((i) => (
+          <li key={i.id}>
+            <a 
+            href={i.link} 
+            style={{ backgroundImage: `url(${i.image})` }}>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Instargram
+export default Instargram;
