5b30916d88bdfb55902295d9fd97ee30a369b7d2
unknown <a99701841@gmail.com>
2026-03-04 18:52:52 +0900

    up

--- src/App.jsx
+++ src/App.jsx
@@ -18,14 +18,25 @@ function App() {
   const [topBanner, setTopBanner] = useState('')
   const [isScrolled, setIsScrolled] = useState(false)
 
+  const [mNavOpen, setMNavOpen] = useState(false)
+
+
+  const handleMNavOpen = () => setMNavOpen(true)
+  const handleMNavClose = () => setMNavOpen(false)
+
   useEffect(() => {
 
     AOS.init({
-      duration:400,
-      easing:'ease'
+      duration: 400,
+      easing: 'ease'
     });
-  },[])
+  }, [])
 
+    useEffect(()=>{
+
+    document.body.style.overflow=mNavOpen?'hidden':''
+
+  },[mNavOpen])
 
   useEffect(() => {
 
@@ -48,7 +59,11 @@ function App() {
     <div className={`app-container ${topBanner} ${isScrolled ? 'scrolled' : ''} `}>
       <FixedTopBtn />
       <TopBanner onClick={upTopBanner} />
-      <Header />
+      <Header
+        mNavOpen={mNavOpen}
+        onNavOpen={handleMNavOpen}
+        onNavClose={handleMNavClose}
+      />
       <main>
         <section id="hero" className='section'>
           <Hero />
